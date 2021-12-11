fbf72f516aa9f5ac4eef025c21eaaf695d4e0231
skelantros <ad3fful@yandex.ru>
2021-12-11 19:51:09 +0300

    Updated Routing and Nav bar

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { Route, Routes } from "react-router";
-import CookieLogin from "../pages/CookieLogin";
+import Auth0LoginPage from "../pages/Auth0LoginPage";
 import WordsPage from "../pages/WordsPage";
+import Profile from "./auth0/Profile";
 
 const AppRouter = () => {
     return(
         <Routes>
-            <Route path="/login" element={<CookieLogin/>} />
+            <Route path="/login" element={<Auth0LoginPage/>} />
             <Route path="/dictionary" element={<WordsPage/>} />
+            <Route path="/profile" element={<Profile/>} />
         </Routes>
     )
 }
--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
         <div className={cl.navbar}>
             <div className={cl.navbarLinks}>
                 <Link to="/login">Логин и регистрация</Link>
+                <Link to="/profile">Профиль</Link>
                 <Link to="/dictionary">Словарь</Link>
             </div>
         </div>
