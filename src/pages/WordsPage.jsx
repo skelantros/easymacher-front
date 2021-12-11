273f5da0142c720560214b2655bc9c10636e2c48
skelantros <ad3fful@yandex.ru>
2021-12-11 19:52:41 +0300

    Update WordsPage.jsx

--- src/pages/WordsPage.jsx
+++ src/pages/WordsPage.jsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react"
 import WordsAPI from "../API/WordsAPI"
 import AddWord from "../components/words/AddWord"
 import WordCard from "../components/words/WordCard"
-import { credsMiddleware } from "../API/middlewares/credsMiddleware"
+import { useAuth0Token } from "../hooks/useAuth0Token"
 import { useFetching } from "../hooks/useFetching"
+import { getWords } from "../API/words"
+import { useAuth0 } from "@auth0/auth0-react"
 
 const WordsPage = () => {
     const [words, setWords] = useState([])
     const [error, setError] = useState('')
 
+    const [getToken] = useAuth0Token()
+
     const [fetchWords, isWordsLoading, wordsError] = useFetching(async () => {
-        const response = await credsMiddleware(WordsAPI.allWords())
-        console.log(response)
+        const token = await getToken()
+        const response = await getWords(token)
         setWords(response.data)
     })
 
