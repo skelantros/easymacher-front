74438c99f6c3f7cbf547a17914aadf7ec1ec812a
skelantros <ad3fful@yandex.ru>
2021-12-11 19:49:20 +0300

    Added Auth0-related dev components

---
+++ src/pages/Auth0LoginPage.jsx
@@ -0,0 +1,13 @@
+import LoginButton from "../components/auth0/LoginButton";
+import LogoutButton from "../components/auth0/LogoutButton";
+
+const Auth0LoginPage = () => {
+    return(
+        <div>
+            <LoginButton/>
+            <LogoutButton/>
+        </div>
+    )
+}
+
+export default Auth0LoginPage;
