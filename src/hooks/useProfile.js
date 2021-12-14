f4e15319fd02ffea0150f610849daddb60cbee00
skelantros <ad3fful@yandex.ru>
2021-12-14 21:36:11 +0300

    Created backend profile hook

---
+++ src/hooks/useProfile.js
@@ -0,0 +1,14 @@
+import { useAuth0Token } from "./useAuth0Token"
+import { getProfile as getBackendProfile } from "../API/users"
+
+export const useBackendProfile = () => {
+    const getToken = useAuth0Token()
+
+    const getProfile = async () => {
+        const token = await getToken()
+        const response = await getBackendProfile(token)
+        return response.data
+    }
+
+    return [getProfile]
+}
