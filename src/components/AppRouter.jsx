33fcfd25fc45834f42b63e7c8aa8f066634ebefd
skelantros <ad3fful@yandex.ru>
2021-12-14 23:25:28 +0300

    Added links to word groups in navbar and router

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route, Routes } from "react-router";
 import Auth0LoginPage from "../pages/Auth0LoginPage";
+import WordGroupsPage from "../pages/WordGroupsPage";
 import WordsPage from "../pages/WordsPage";
 import Profile from "./auth0/Profile";
 
@@ -10,6 +11,7 @@ const AppRouter = () => {
             <Route path="/login" element={<Auth0LoginPage/>} />
             <Route path="/dictionary" element={<WordsPage/>} />
             <Route path="/profile" element={<Profile/>} />
+            <Route path="/word-groups" element={<WordGroupsPage/>} />
         </Routes>
     )
 }
