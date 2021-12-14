a27291dd89abd19cab35534f5dd6021817cc2d67
skelantros <ad3fful@yandex.ru>
2021-12-14 21:57:20 +0300

    Added exports of word groups requests

--- src/API/wordGroups.js
+++ src/API/wordGroups.js
@@ -1,10 +1,10 @@
 import { bearerConfig, getRequest, postRequest } from "./instance"
 
-const getWordGroups = (token) => {
+export const getWordGroups = (token) => {
     return getRequest("/word-groups", bearerConfig(token))
 }
 
-const getWordGroupsByOwner = (token, ownerId) => {
+export const getWordGroupsByOwner = (token, ownerId) => {
     return getRequest("/word-groups", {
         params: {
             user: ownerId
@@ -13,30 +13,30 @@ const getWordGroupsByOwner = (token, ownerId) => {
     })
 }
 
-const getWordGroup = (token, id) => {
+export const getWordGroup = (token, id) => {
     return getRequest(`/word-groups/${id}`, bearerConfig(token))
 }
 
-const createWordGroup = (token, name, isShared) => {
+export const createWordGroup = (token, name, isShared) => {
     return postRequest("/word-groups/create", {
         name,
         isShared
     }, bearerConfig(token))
 }
 
-const addWordsToGroup = (token, groupId, wordsIds) => {
+export const addWordsToGroup = (token, groupId, wordsIds) => {
     return postRequest(`/word-groups/${groupId}/add-words`, {
         words: wordsIds
     }, bearerConfig(token))
 }
 
-const updateGroup = (token, id, name = null, isShared = null) => {
+export const updateGroup = (token, id, name = null, isShared = null) => {
     return postRequest(`/word-groups/${id}/update`, {
         isShared,
         name
     }, bearerConfig(token))
 }
 
-const removeGroup = (token, id) => {
+export const removeGroup = (token, id) => {
     return postRequest(`/word-groups/${id}/remove`, {}, bearerConfig(token))
 }
