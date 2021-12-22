5683aabe487e150b5648a5ee1d9e172ecb628ade
skelantros <ad3fful@yandex.ru>
2021-12-22 18:16:37 +0300

    Updated guess related pages

--- src/components/guess/GuessResult.jsx
+++ src/components/guess/GuessResult.jsx
@@ -1,4 +1,6 @@
-import EMButton from "../UI/button/EMButton";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 import WordCard from "../words/WordCard"
 
 const GuessResult = ({score, wrongWords, endCallback, restartCallback}) => {
@@ -13,14 +15,17 @@ const GuessResult = ({score, wrongWords, endCallback, restartCallback}) => {
     }
 
     return(
-        <div>
-            <p>Вы ответили правильно на <b>{score}</b> вопросов.</p>
-            <p>Список слов, на которые вы ответили неправильно:</p>
-            {wrongWords.map(w => <div><WordCard key={w.id} word={w}/><p/></div>)}
-            <p/>
-            <EMButton onClick={confirmEnd}>Завершить</EMButton>
-            <EMButton onClick={confirmRestart}>Заново</EMButton>
-        </div>
+        <Container>
+            <Row>Вы ответили правильно на {score} вопросов.</Row>
+            <Row>Список слов, на которые вы ответили неправильно:</Row>
+            <Row>
+                {wrongWords.map(w => <WordCard key={w.id} word={w}/>)}
+            </Row>
+            <Row>
+                <Button onClick={confirmEnd} className="mt-1" style={{ width: '8rem' }}>Завершить</Button>
+                <Button onClick={confirmRestart} className="mx-2 mt-1" style={{ width: '8rem' }}>Заново</Button>
+            </Row>
+        </Container>
     )
 }
 
